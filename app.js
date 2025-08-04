6f8928770a9eaf5e95c7104e1810b63b26b02457
Arthur <arthurborba271@gmail.com>
2025-08-04 11:04:44 -0300

    inicial

---
+++ app.js
@@ -0,0 +1,2 @@
+const calc = request('./calculadora');
+console.log("soma : ", calc.soma(1,4));
---
+++ calculadora.js
@@ -0,0 +1,5 @@
+const soma = (a,b) => {a+b;}
+
+const multiplicar = (a,b) => {a*b;}
+
+module.exports = {soma, multiplicar}
